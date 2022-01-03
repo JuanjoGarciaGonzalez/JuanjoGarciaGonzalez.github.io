77547d3c4935eaf9d12d3e3e12a0c22ed95e1b51
AdriánDW32 <agarciago20dw@ikzubirimanteo.com>
2022-01-03 16:41:30 +0100

    Cambios en habilidades y foto de perfil 3

--- index.php
+++ index.php
@@ -77,9 +77,9 @@
                         <img src="assets/Images/perfil.png" alt="Foto de perfil">
                     </div>
                     <div class="texto-modal">
-                        <p style="color: #436280; background-color: #dbe4ed;">Vivo en Lasarte-Oria (San Sebastián), tengo 22 años y he estudiado un certificado de profesionalidad de <strong>APLICACIONES CON TECNOLOGÍAS WEB (IFCD0210)</strong>. He aprendido habilidades frontend, backend, despliegue de sitios web, accesibilidad, usabilidad... Además practico bastante por mi cuenta ya que me gusta lo que hago. Antes de esto yo estudié un grado medio de <strong>SISTEMAS MICROINFORMÁTICOS Y REDES</strong> dónde me adentré en el mundo de los ordenadores.</p>
+                        <p style="color: #436280;">Vivo en Lasarte-Oria (San Sebastián), tengo 22 años y he estudiado un certificado de profesionalidad de <strong>APLICACIONES CON TECNOLOGÍAS WEB (IFCD0210)</strong>. He aprendido habilidades frontend, backend, despliegue de sitios web, accesibilidad, usabilidad... Además practico bastante por mi cuenta ya que me gusta lo que hago. Antes de esto yo estudié un grado medio de <strong>SISTEMAS MICROINFORMÁTICOS Y REDES</strong> dónde me adentré en el mundo de los ordenadores.</p>
     
-                        <p style="color: #436280; background-color: #dbe4ed">Me gustaría conseguir mi primer trabajo de desarrollador web e ir aprendiendo poco a poco más en profundidad este mundo maravilloso y extenso.</p>
+                        <p style="color: #436280;">Me gustaría conseguir mi primer trabajo de desarrollador web e ir aprendiendo poco a poco más en profundidad este mundo maravilloso y extenso.</p>
                     </div>
                 </div>
               </div>
