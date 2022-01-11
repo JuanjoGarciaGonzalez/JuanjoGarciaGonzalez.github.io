90950cdda26fcce4ea197d3ae8cca1103df3c152
adrikbd99 <adrikbd99@gmail.com>
2022-01-11 02:20:32 +0100

    Eliminado el enlace de jquery validate

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
     <!-- jQuery Modal -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
-    <!-- VALIDATE -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
     <!-- ESTILOS PROPIOS -->
     <link rel="stylesheet" href="assets/CSS/estilos-desktop.css">
     <link rel="stylesheet" href="assets/CSS/estilos-tablet.css">
